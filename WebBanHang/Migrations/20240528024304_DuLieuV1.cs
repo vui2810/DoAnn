7a7740b479ea0d276288021d612b125350db6674
vui2810 <chauminhvui2004@gmail.com>
2024-06-26 20:58:51 +0700

    Add project files.

---
+++ WebBanHang/Migrations/20240528024304_DuLieuV1.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebBanHang.Migrations
+{
+    public partial class DuLieuV1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[] { 1, 1, "Điện thoại" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[] { 2, 2, "Máy tính bảng" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[] { 3, 3, "Laptop" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, 1, null, null, "SH350i", 300.0 },
+                    { 2, 1, null, null, "SH160i/125i", 350.0 },
+                    { 3, 1, null, null, "Sh Mode 125", 400.0 },
+                    { 4, 1, null, null, "Vario 160", 420.0 },
+                    { 5, 1, null, null, "Air Blade 2025", 630.0 },
+                    { 6, 1, null, null, "Vario 125", 750.0 },
+                    { 7, 1, null, null, "LEAD 125", 820.0 },
+                    { 8, 1, null, null, "Iphone 14 Pro", 950.0 },
+                    { 9, 1, null, null, "Vision Phiên Bản C", 1200.0 },
+                    { 10, 1, null, null, "Super Cub C125 ", 1450.0 },
+                    { 11, 2, null, null, "Wave RSX", 750.0 },
+                    { 12, 2, null, null, "Wave Alpha 110", 1250.0 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
---
+++ WebBanHang/Models/ApplicationDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace WebBanHang.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        }
+        public DbSet<Category> Categories { set; get; }
+        public DbSet<Product> Products { set; get; }
+        public DbSet<Order> Orders { set; get; }
+        public DbSet<OrderDetail> OrderDetails { set; get; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //seed data to table Categories
+            modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Yamaha", DisplayOrder = 1 },
+            new Category { Id = 2, Name = "Piaggio", DisplayOrder = 2 },
+            new Category { Id = 3, Name = "Honda", DisplayOrder = 3 });
+            //seed data to table Product
+            modelBuilder.Entity<Product>().HasData(
+            new Product { Id = 1, Name = "SH350i", Price = 300, CategoryId = 1 },
+            new Product { Id = 2, Name = "SH160i/125i", Price = 350, CategoryId = 1 },
+            new Product { Id = 3, Name = "Sh Mode 125", Price = 400, CategoryId = 1 },
+            new Product { Id = 4, Name = "Vario 160", Price = 420, CategoryId = 1 },
+            new Product { Id = 5, Name = "Air Blade 2025", Price = 630, CategoryId = 1 },
+            new Product { Id = 6, Name = "Vario 125", Price = 750, CategoryId = 1 },
+            new Product { Id = 7, Name = "LEAD 125", Price = 820, CategoryId = 1 },
+            new Product { Id = 8, Name = "Vision Phiên Bản C", Price = 950, CategoryId = 1 },
+            new Product { Id = 9, Name = "Super Cub C125", Price = 1200, CategoryId = 1 },
+            new Product { Id = 10, Name = "Future 125 FI ", Price = 1450, CategoryId = 1 },
+            new Product { Id = 11, Name = "Blade 2023", Price = 750, CategoryId = 2 },
+            new Product { Id = 12, Name = "Wave Alpha 110", Price = 1250, CategoryId = 2 });
+        }
+
+
+    }
+}
